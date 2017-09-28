dd21f743edfe86e1a60a46bf8c22a4bb70ecb8d7
Pavel <kleshnin.p.o@gmail.com>
2017-09-28 20:58:15 +0300

    +dreadsbymohh

---
+++ dreadsbymohh/index.php
@@ -0,0 +1,312 @@
+<!DOCTYPE HTML>
+<!--
+	Miniport by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+
+<head>
+    <title>Dreads by Mohh - Добрые дреды в Москве</title>
+    <meta charset="utf-8" />
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name='yandex-verification' content='45096374b8bbd4af' />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="Keywords" content="дреды москва, дреды, Mohh, dreads by mohh, Мохх, dreads, dreadlocks, расплетение, заплетение, подплетение, дредмастер, сделать дреды, dreads moscow, сделать дреды, как заплести дреды, где заплести дреды, дреды дешево, дреды в москве">
+    <meta name="description" content="Дред-мастера Dreads by Mohh в Москве - мы сделаем вам действительно качественные дреды, о которых вам не придется волноваться">
+    <meta name='wmail-verification' content='27568f8c1ac6ee8e0cafa59124e55de7' />
+    <meta http-equiv="expires" content="31556926">
+    <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+    <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+    <!--FOTORAMA-->
+    <script async src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <!-- Fotorama -->
+    <!--GoogleAnalyticsObject-->
+    <script>
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-80720792-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+    <script src='https://www.google.com/recaptcha/api.js?hl=ru' async defer></script>
+    <!--CAPTCHA GOOGLE-->
+    <!--GoogleAnalyticsObject-->
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function(d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter38501155 = new Ya.Metrika({
+                        id: 38501155,
+                        clickmap: true,
+                        trackLinks: true,
+                        accurateTrackBounce: true
+                    });
+                } catch (e) {}
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function() {
+                    n.parentNode.insertBefore(s, n);
+                };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else {
+                f();
+            }
+        })(document, window, "yandex_metrika_callbacks");
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/38501155" style="position:absolute; left:-9999px;" alt="" />
+        </div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
+    <link href="assets/fotorama/fotorama.css" type="text/css" rel="stylesheet" />
+    <script async src="assets/fotorama/fotorama.js" type="text/javascript"></script>
+</head>
+
+<body>
+    <!-- Nav -->
+    <nav id="nav">
+        <ul class="container">
+            <li><a href="#top">О нас</a>
+            </li>
+            <li><a href="#portfolio">Портфолио</a>
+            </li>
+            <li><a href="#price">Цены</a>
+            </li>
+            <li><a href="#contact">Контакты</a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- Home -->
+    <div class="wrapper style1 first">
+        <article class="container" id="top">
+            <div class="row">
+                <div class="4u 12u(mobile)">
+                    <span class="image fit"><img src="images/pic1000.jpg" alt="Dreads by Mohh" /></span>
+                </div>
+                <div class="8u 12u(mobile)">
+                    <header>
+                        <h1>Привет! Мы <strong>Моххи</strong>.</h1>
+                    </header>
+                    <h2>И мы сделаем вам дреды :)</h2>
+                    <p>Качественные, ровные и аккуратные дреды, с которыми вам не придется подплетаться раз в месяц. Именно такие дреды, какие хочется вам. Такие дреды, которым будут завидовать абсолютно все. Мы гарантируем, что вы останетесь довольны нашей работой! </p>
+                    <a href="#contact" class="button big scrolly">Записаться на сеанс</a>
+                </div>
+            </div>
+        </article>
+    </div>
+
+
+    <!-- Portfolio -->
+    <div class="wrapper style3">
+        <article id="portfolio">
+            <header>
+                <h2>Здесь вы можете увидеть некоторые наши работы</h2>
+                <p>Еще больше работ вы можете оценить в нашей <a href="https://vk.com/dreadsbymohh" target="_blank">группе Вконтакте</a> </p>
+            </header>
+            <!--Fotorama-->
+            <div class="fotorama" data-width="700" data-ratio="700/467" data-max-width="100%" data-nav="thumbs">
+                <img src="images/pic00.jpg" alt="image" />
+                <img src="images/pic01.jpg" alt="image" />
+                <img src="images/pic02.jpg" alt="image" />
+                <img src="images/pic03.jpg" alt="image" />
+                <img src="images/pic04.jpg" alt="image" />
+                <img src="images/pic05.jpg" alt="image" />
+                <img src="images/pic06.jpg" alt="image" />
+                <img src="images/pic07.jpg" alt="image" />
+                <img src="images/pic08.jpg" alt="image" />
+                <img src="images/pic09.jpg" alt="image" />
+                <img src="images/pic10.jpg" alt="image" />
+                <img src="images/pic11.jpg" alt="image" />
+                <img src="images/pic12.jpg" alt="image" />
+                <img src="images/pic13.jpg" alt="image" />
+                <img src="images/pic14.jpg" alt="image" />
+                <img src="images/pic15.jpg" alt="image" />
+                <img src="images/pic16.jpg" alt="image" />
+                <img src="images/pic17.jpg" alt="image" />
+                <img src="images/pic18.jpg" alt="image" />
+                <img src="images/pic19.jpg" alt="image" />
+                <img src="images/pic20.jpg" alt="image" />
+            </div>
+
+            <footer>
+                <p>До сих пор сомневаетесь?</p>
+                <a href="#price" class="button big scrolly">Тогда взгляните на наши цены!</a>
+            </footer>
+        </article>
+    </div>
+
+    <!-- Цены -->
+    <div class="wrapper style2">
+        <article id="price">
+            <header>
+                <h2>Наши цены</h2>
+                <p></p>
+            </header>
+            <div class="container">
+                <div class="row">
+                    <div class="4u 12u(mobile)">
+                        <section class="box style1">
+                            <img src="images/thumb1.jpg" alt="Натуральные дреды, подплетение" class="round">
+                            <h3>Натуральные дреды, подплетение</h3>
+                            <p>1500р/час</p>
+                            <h3>Коррекция</h3>
+                            <p>1500р/час</p>
+                            <h3>Удлинение</h3>
+                            <p>1500р/час</p>
+                        </section>
+                    </div>
+                    <div class="4u 12u(mobile)">
+                        <section class="box style1">
+                            <img src="images/thumb3.jpg" alt="Обучение" class="round">
+                            <h3>Обучение<br> всем видам дред</h3>
+                            <p>Индивидуально - 8500р
+                                <br>В группе - 5000р</p>
+                            <h3>Обучение одному виду дред</h3>
+                            <p>Индивидуально - 5000р
+                                <br>В группе - 3000р</p>
+                        </section>
+                    </div>
+                    <div class="4u 12u(mobile)">
+                        <section class="box style1">
+                            <img src="images/thumb2.jpg" alt="Безопасные дреды" class="round">
+                            <h3>Безопасные дреды (Только изготовление) <br> До 30см</h3>
+                            <p>4500р + материал(~800р)</p>
+                            <h3>40-45см</h3>
+                            <p>5000 + материал(~1200р)</p>
+                            <h3>50-60см</h3>
+                            <p>6500р + материал(~1600р)</p>
+                        </section>
+                    </div>
+
+                </div>
+            </div>
+            <footer>
+
+                <p>Все еще не можете решиться?</p>
+                <a href="#portfolio" class="button big scrolly">Взгляните еще раз на наши работы :)</a>
+            </footer>
+        </article>
+    </div>
+
+
+    <!-- Contact -->
+    <div class="wrapper style4">
+        <article id="contact" class="container 75%">
+            <header>
+                <h2>Контакты</h2>
+                <p>Мы работаем в г.Москва, в двух минутах от метро Жулебино.
+                    <br> Звоните, пожалуйста, с 11-00 до 22-00.</p>
+                <ul>
+                    <li><a href="tel:7-917-551-97-91">Паша +7(917)551-97-91</a>
+                    </li>
+                    <li><a href="tel:7-917-551-97-91">Настя +7(916)451-48-50</a>
+                    </li>
+                </ul>
+                <p>Заполните эту мини-анкету, и мы свяжемся с вами как можно скорее!</p>
+
+            </header>
+            <div>
+                <div class="row">
+                    <div class="12u">
+                    		
+                        <form method="post" action="mail.php">
+                            <div>
+                                <div class="row">
+                                    <div class="6u 12u(mobile)">
+
+                                        <input type="text" name="name" id="name" placeholder="Имя" />
+                                    </div>
+                                    <div class="6u 12u(mobile)">
+                                        <input type="text" name="email" id="email" placeholder="Email" />
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="12u">
+                                        <input type="text" name="vk" id="id" placeholder="Ваша страница Вконтакте" />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="12u">
+                                        <textarea name="message" id="message" placeholder="Комментарий"></textarea>
+                                    </div>
+                                </div>
+                                <div class="row 200%">
+                                    <div class="12u">
+                                        <ul class="actions">
+                                            <li>
+                                                <input type="submit" value="Записаться!" />
+                                            </li>
+                                            <li>
+                                                <input type="reset" value="Очистить анкету" class="alt" />
+                                            </li>
+                                        </ul>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                       
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="12u">
+                        <hr />
+                        <h3>Мы в соцсетях:</h3>
+
+                        <ul class="social-list">
+                            <li class="social-item vk">
+                                <a href="vk.com/dreadsbymohh"></a>
+                            </li>
+                            <li class="social-item inst">
+                                <a href="instagram.com/"></a>
+                            </li>
+                        </ul>
+                        <hr />
+                    </div>
+                </div>
+            </div>
+            <footer>
+                <ul id="copyright">
+                    <li>&copy; Dreads by Mohh 2017. All rights reserved.</li>
+                    <li>Design: <a href="http://html5up.net">HTML5 UP</a>
+                    </li>
+                </ul>
+            </footer>
+        </article>
+    </div>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.scrolly.min.js"></script>
+    <script src="assets/js/skel.min.js"></script>
+    <script src="assets/js/skel-viewport.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
---
+++ dreadsbymohh/mail.php
@@ -0,0 +1,105 @@
+<? 
+// ----------------------------конфигурация-------------------------- // 
+
+$adminemail="kleshnin.p.o@gmail.com";  // e-mail админа 
+ 
+ 
+$date=date("d.m.y"); // число.месяц.год 
+ 
+$time=date("H:i"); // часы:минуты:секунды 
+ 
+$backurl="http://dreadsbymohh.ru/index.php";  // На какую страничку переходит после отправки письма 
+ 
+//---------------------------------------------------------------------- // 
+ 
+  
+ 
+// Принимаем данные с формы 
+ 
+$name=$_POST['name']; 
+ 
+$email=$_POST['email']; 
+ 
+$vk=$_POST['vk'];
+
+$msg=$_POST['message'];
+
+$res=$_POST['res'];
+
+
+ 
+  
+ 
+// Проверяем валидность e-mail 
+ 
+if (!preg_match("|^([a-z0-9_\.\-]{1,20})@([a-z0-9\.\-]{1,20})\.([a-z]{2,4})|is", 
+strtolower($email))) 
+ 
+ { 
+ 
+  echo 
+"<center>Вернитесь <a 
+href='javascript:history.back(1)'><B>назад</B></a>. Вы 
+указали неверные данные!"; 
+ 
+  } 
+ 
+ else 
+ 
+ { 
+ 
+ 
+$msg=" 
+ 
+ 
+<p>Имя: $name</p> 
+ 
+ 
+<p>E-mail: $email</p> 
+
+<p>VK: $vk</p>  
+ 
+<p>Сообщение: $msg</p> 
+ 
+ 
+"; 
+ 
+  
+ 
+ // Отправляем письмо админу  
+ 
+mail("kleshnin.p.o@gmail.com", "$date $time Сообщение 
+от $name", "$msg"); 
+ 
+  
+ 
+// Сохраняем в базу данных 
+ 
+$f = fopen("message.txt", "a+"); 
+ 
+fwrite($f," \n $date $time Сообщение от $name"); 
+ 
+fwrite($f,"\n $msg "); 
+ 
+fwrite($f,"\n ---------------"); 
+ 
+fclose($f); 
+ 
+  
+ 
+// Выводим сообщение пользователю 
+ 
+print "<script language='Javascript'><!-- 
+function reload() {location = \"$backurl\"}; setTimeout('reload()', 3000); 
+//--></script> 
+ 
+$msg 
+ 
+<p>Сообщение отправлено! Подождите, сейчас вы будете перенаправлены на главную страницу...</p>";  
+exit; 
+ 
+ } 
+?>
+
+
+
