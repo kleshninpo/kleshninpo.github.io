879b5e359d69580b7127b3035e62d9440fb86c08
Pavel <kleshnin.p.o@gmail.com>
2017-09-28 20:15:09 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+$(document).ready(function() {
+	$(".header-link").click( function() {
+		var scroll_el = $(this).attr("href");
+		if($(scroll_el).length != 0) {
+			$("html, body").animate({
+				scrollTop: $(scroll_el).offset().top
+			}, 500);
+		}
+		return false;
+	})
+});
+
 $( window ).scroll( function() {
 	if ( $( this ).scrollTop() > 200) {
 		$('header').addClass("scrolled");
@@ -11,3 +23,4 @@ $( window ).scroll( function() {
 $(".menu-icon").click( function() {
 	$(".page-header__nav-list").toggleClass("mobile")
 });
+
